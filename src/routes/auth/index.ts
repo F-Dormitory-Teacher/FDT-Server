436e5c62690848c1e8c822e8835942e9cb435388
_uchanlee <uchanlee.dev@gmail.com>
2020-11-24 04:09:32 +0900

    add: login routing

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -4,6 +4,7 @@ import authController from "../../controllers/auth.controller";
 const router = Router();
 
 router.get("/", authController.certifyAuthCode);
+router.post("/", authController.login);
 router.post("/register", authController.register);
 router.post("/auth-code", authController.sendAuthCode);
 
